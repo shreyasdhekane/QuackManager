6212d69a1b2458ec3082f0bc146b1c992da20cdc
shreyasdhekane <sdhekane@iu.edu>
2026-03-01 11:57:03 -0500

    Initial commit

---
+++ src/test/java/edu/p532/project0/Project0ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.p532.project0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
